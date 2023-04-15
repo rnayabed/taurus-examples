3ddf1edc561b4fb8491343d354205c914c69fd32
Debayan Sutradhar <debayansutradhar3@gmail.com>
2023-04-15 18:00:38 +0530

    update led_toggle

--- gpio/led_toggle/led_toggle_pgm.c
+++ gpio/led_toggle/led_toggle_pgm.c
@@ -30,7 +30,6 @@
 
 #include "stdlib.h"
 #include "config.h"
-#include "led.h"
 #include "gpio.h"
 
 
@@ -40,23 +39,26 @@
  * @param[in] No input parameter 
  * @param[Out] No output parameter 
 */
-void main2()
+void main()
 {
 	US sw_status = 0;
 	UI i = 0;
 	printf("\n\r Please check the LD5,LD6 and LD7 status on Dev. Board");
+
+	GPIO_set_pin_mode(PIN_25, OUT);
+	
 	while(1)
 	{
 		//Turn ON LEDs 5, 6, 7 at GPIO pins 16,17,18 respectively.
 		
 		
-		on_LED(PIN_25);
+		GPIO_write_pin(PIN_25, HIGH);
 		printf("\n\r LEDs ON");
 		for(i=0; i< 0x800000;i++); // Delay
 
 		//Turn OFF LEDs 5, 6, 7 at GPIO pins 16,17,18 respectively.
 
-		off_LED(PIN_25);
+		GPIO_write_pin(PIN_25, LOW);
 		printf("\n\r LEDs OFF");
 
 		for(i=0; i< 0x800000;i++); // Delay
